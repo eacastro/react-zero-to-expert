84c87f99112166438785878a458920f2cf8451f1
Edisson Castro <edison.castro@arra-in.co>
2024-06-25 14:24:26 -0500

    Imports, exports and some array functions

---
+++ src/fundamentals/08-import-export-array-functions.js
@@ -0,0 +1,24 @@
+// ======================================================
+// IMPORT, EXPORT Y FUNCIONES COMUNES DE ARREGLOS
+// ======================================================
+
+// Importar archivo
+import heroes from '../data/heroes'; // Path relativo al archivo donde estamos
+
+console.log('heroes', heroes);
+
+// Método Find:
+
+const getHeroById = (id) => {
+  // find recorre todos los elementos del arreglo y devuelve los
+  // elementos que satisfagan la condición devuelta en el callback
+  return heroes.find((heroe) => heroe.id === id);
+}
+
+console.log(getHeroById(1));
+
+// Mëtodo Filter:
+const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner); // Filter permite obtener los valores que cumplan con la condición dada.
+
+console.log(getHeroesByOwner('Dc'));
+
