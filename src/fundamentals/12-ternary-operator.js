e9c7dd16ee6f0875a2b587824f7040d6aac6fb8a
Edisson Castro <edison.castro@arra-in.co>
2024-06-25 22:56:19 -0500

    Ternary operator

---
+++ src/fundamentals/12-ternary-operator.js
@@ -0,0 +1,17 @@
+// ===============================
+// Operador condicional ternario
+// ===============================
+
+const active = true;
+// let message = '';
+
+// if (active) {
+//     message = 'Active User';
+// } else {
+//     message = 'Inactive User';
+// }
+
+// const message = (active) ? 'Active User' : 'Inactive User';
+const message = active && 'Activo';
+
+console.log(message);
